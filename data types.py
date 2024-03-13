e72f76c0c35983f8d5e97bd1c9c5b6ccfe50bd3c
Zargham Abbas <zarghamabbas859@gmail.com>
2024-03-13 20:46:42 +0500

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,31 @@
+# Zargham=abbas
+# zarghamabbas859@gmail.com
+
+ #Question_5:
+#Write a Python program that return all datatypes that we discussed in the class.
+
+# Declare the desired variables for each Datatypes program and declare print function for each datatype program.
+
+num1 = 5
+print(num1, 'is of type', type(num1))
+
+num2 = 2.0
+print(num2, 'is of type', type(num2))
+
+num4 =True
+print(num4, 'is of type', type(num4))
+
+num5 =('a+b')
+print(num5, 'is of type', type(num5))
+
+num6 =['a+b',1.5,True]
+print(num6, 'is of type', type(num6))
+
+num7 =('zargham',1.5,False)
+print(num7, 'is of type', type(num7))
+
+num8 ={'password':'login','enter':'username'}
+print(num8, 'is of type', type(num8))
+
+# RUN ALL DATATYPE PROGRAM.
+# DONE!
